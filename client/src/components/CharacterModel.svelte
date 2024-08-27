4dc1b621a5ecd79e93e8138a16c1c815d7b8f835
Veikkan <pierre.semanne@gmail.com>
2024-08-27 17:10:42 +0200

    feat: add some checks for charact cam

--- client/src/components/CharacterModel.svelte
+++ client/src/components/CharacterModel.svelte
@@ -3,7 +3,6 @@
     import { setupStore } from "src/main";
     import { createComponentValueStore } from "src/dojo/componentValueStore";
     import { derived } from "svelte/store";
-    
     export let id = 0;
     let entity: any;
     let character
@@ -17,15 +16,30 @@
         : undefined
     );
 
+    let account: any;
+ 
+    $: if (burnerManager) account = burnerManager.getActiveAccount();
     $: character = createComponentValueStore(clientComponents.Character, entity);
     $: position = createComponentValueStore(clientComponents.Position, entity);
+
+    function AddressToBigInt(address) {
+        // Ensure the address starts with '0x'
+        if (!address.startsWith('0x')) {
+            address = '0x' + address;
+        }
+        // Convert hexadecimal address to BigInt
+        return BigInt(address);
+    }
 </script>
 
-{#if $character && $position}
-<T.Mesh position={[$position.x/ 100 - 51, 0, $position.y / 100 - 51]}>
-    <T.BoxGeometry/>
-    <T.MeshStandardMaterial color="red"/>
-</T.Mesh>
+{#if $character && $position && account}
+    {#if $character.player_id === AddressToBigInt(account.address)}
+        <T.PerspectiveCamera position={[$position.x / 100 - 51, 0.5, $position.y / 100 - 51]} />
+    {/if}
+    <T.Mesh position={[$position.x / 100 - 51, 0.5, $position.y / 100 - 51]}>
+        <T.BoxGeometry />
+        <T.MeshStandardMaterial color="red" />
+    </T.Mesh>
 {/if}
 
 
