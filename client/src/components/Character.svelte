366016510b91461d7f0fb424061ca01d534eadc9
Veikkan <pierre.semanne@gmail.com>
2024-08-27 18:14:50 +0200

    feat: added zachs stuff

--- client/src/components/Character.svelte
+++ client/src/components/Character.svelte
@@ -1,126 +1,233 @@
 <script lang="ts">
-    import type { RigidBody as RapierRigidBody } from '@dimforge/rapier3d-compat'
-    import { T, useTask, useThrelte } from '@threlte/core'
-    import { RigidBody, CollisionGroups, Collider } from '@threlte/rapier'
-    import { onDestroy } from 'svelte'
-    import { PerspectiveCamera, Vector3 } from 'three'
-    import PointerLockControls from './PointerLockControls.svelte'
-    import Gun from './Gun.svelte'
+  import { T, useFrame, useThrelte } from '@threlte/core'
+import { RigidBody, CollisionGroups, Collider } from '@threlte/rapier'
+import { PerspectiveCamera, Vector2, Vector3 } from 'three'
+import PointerLockControls from './PointerLockControls.svelte'
+import Gun from './Gun.svelte'
+  import type { RigidBody as RapierRigidBody } from '@dimforge/rapier3d-compat'
+  import { writable } from 'svelte/store'
+  import { Bullet } from 'src/dojo/typescript/models.gen'
+  import * as THREE from 'three'
+//  import { bullets, characters } from 'src/stores'
+
+export let position: [number, number, number] = [0, 0, 0]
+
+  interface Bullet {
+    x: number;
+    y: number;
+    direction: THREE.Quaternion;
+    speed: number;
+    id: number;
+  }
+
+  let rigidBody: RapierRigidBody
+  let cam: PerspectiveCamera
+  let moveDirection = new Vector3()
+  let speed = 5
+
+  let previous_move = [0,0,0];
+  let moves: any[] = [];
+
+  let interval = .05;
+  let turn_length = 600;
+  let isMouseDown = false;
+
+  let worldPosition = new THREE.Vector3();
+
+
+
+  let bullets:any[] = [];
+  let cooldown = 0;
+  let frame_counter = 0;
+
+  $: progressWidth = Math.min(frame_counter/300 * 100, 100);
+
+
+  const keyState = {
+      forward: false,
+      backward: false,
+      left: false,
+      right: false,
+  }
+
+  function handleKeyDown(event: KeyboardEvent) {
+      switch (event.key.toLowerCase()) {
+          case 'w': keyState.forward = true; break
+          case 's': keyState.backward = true; break
+          case 'a': keyState.left = true; break
+          case 'd': keyState.right = true; break
+      }
+  }
+
+  function handleKeyUp(event: KeyboardEvent) {
+      switch (event.key.toLowerCase()) {
+          case 'w': keyState.forward = false; break
+          case 's': keyState.backward = false; break
+          case 'a': keyState.left = false; break
+          case 'd': keyState.right = false; break
+      }
+  }
+
+  function handleMouseDown(event: MouseEvent) {
+  if (event.button === 0) { // Left mouse button
+      isMouseDown = true;
+  }
+    }
+
+    function handleMouseUp(event: MouseEvent) {
+        if (event.button === 0) { // Left mouse button
+            isMouseDown = false;
+        }
+    }
+    let turn_over = false;
+    function truncateToDecimals(num: number, decimalPlaces:number) {
+        const multiplier = Math.pow(10, decimalPlaces);
+      return Math.floor(num * multiplier) / multiplier;
+    }
+
+  useFrame((_, delta) => {
+      if (!rigidBody || !cam) return        
+      moveDirection.set(0, 0, 0)
+
+      if (keyState.forward) moveDirection.z -= 1
+      if (keyState.backward) moveDirection.z += 1
+      if (keyState.left) moveDirection.x -= 1
+      if (keyState.right) moveDirection.x += 1
+
+      if (moveDirection.length() > 0) {
+        frame_counter += 1;
+
+        moveDirection.normalize().multiplyScalar(speed)
+        moveDirection.applyQuaternion(cam.quaternion);
+        moveDirection.x = truncateToDecimals(moveDirection.x, 3);
+        moveDirection.z = truncateToDecimals(moveDirection.z, 3);
+
+        if (frame_counter % 3 == 0 && !turn_over) {
+          if (cooldown > 0){
+            cooldown -=1;
+          }
+          if (frame_counter == 300) {
+            turn_over = true;
+            document.exitPointerLock();
+            console.log(moves)
+            console.log(bullets)
+
+          }
+          if (isMouseDown) {
+            if ( bullets.length < 5 && cooldown == 0) {
+              //TODO make sure to normailze so that (0,0) is corner rather than center
+              let cam_position = cam.getWorldPosition(worldPosition).clone()
+              cam_position.x = truncateToDecimals(cam_position.x, 2);
+              cam_position.z = truncateToDecimals(cam_position.z, 2);
+              console.log(cam_position)
+
+              let position: [number, number] = [cam_position.x, cam_position.z]
+              let bullet: Bullet = {x: position[0], y: position[1], direction: cam.quaternion.clone(), speed: 25, id:(frame_counter%3)};
+              console.log(bullet)
+
+              bullets.push(bullet);
+              
+              cooldown = 5;
+            }
+          }
+
+          let move = {x: moveDirection.x, y: moveDirection.y}
+          moves.push(move);
+
+          const currentVel = rigidBody.linvel()
+          rigidBody.setLinvel({ x: moveDirection.x, y: currentVel.y, z: moveDirection.z }, true)
+
+          // Update position for camera and other components
+          const pos = rigidBody.translation()
+          position = [pos.x, pos.y, pos.z]
+
+      // Update bullets
+      for (let i = 0; i < bullets.length; i++) {
+        let bullet = bullets[i];
+        
+        // Calculate the displacement
+        //TODO use fast_sin / fast_cos to match cairo logic
+        
+        
+        // Update the bullet's position
+        bullet.x += truncateToDecimals(bullet.direction.fast_cos * bullet.velocity * delta, 2);
+        bullet.y += truncateToDecimals(bullet.direction.fast_sin * bullet.velocity * delta,2);
+        
+
+        // Optional: Remove bullets that have traveled too far
+        if (Math.sqrt(bullet.x**2 + bullet.y**2) > 1000) { // Adjust this value as needed
+          bullets.splice(i, 1);
+          i--;
+        }
+      }
+    }
+      }
+  })
+        
+
+  const { renderer } = useThrelte()
   
-
-    export let position: [x: number, y: number, z: number] = [0, 0, 0]
-    let radius = 0.3
-    let height = 1.7
-    export let speed = 10
-    let rigidBody: RapierRigidBody
-    let lock: () => void
-    let cam: PerspectiveCamera
-    let forward = 0
-    let backward = 0
-    let left = 0
-    let right = 0
-    const t = new Vector3()
-    const lockControls = () => lock()
-    const { renderer } = useThrelte()
-
-    renderer.domElement.addEventListener('click', lockControls)
-
-    onDestroy(() => {
-      renderer.domElement.removeEventListener('click', lockControls)
-    })
-
-    useTask(() => {
-      if (!rigidBody) return
-      // get direction
-      const velVec = t.fromArray([right - left, 0, backward - forward])
-      // sort rotate and multiply by speed
-      velVec.applyEuler(cam.rotation).multiplyScalar(speed)
-      // don't override falling velocity
-      const linVel = rigidBody.linvel()
-      t.y = linVel.y
-      // finally set the velocities and wake up the body
-      rigidBody.setLinvel(t, true)
-      // when body position changes update position prop for camera
-      const pos = rigidBody.translation()
-      position = [pos.x, pos.y, pos.z]
-    })
-    function onKeyDown(e: KeyboardEvent) {
-      switch (e.key) {
-        case 's':
-          backward = 1
-          break
-        case 'w':
-          forward = 1
-          break
-        case 'a':
-          left = 1
-          break
-        case 'd':
-          right = 1
-          break
-        default:
-          break
+  function lockControls() {
+      if (document.pointerLockElement !== renderer.domElement) {
+          renderer.domElement.requestPointerLock()
       }
-    }
-    function onKeyUp(e: KeyboardEvent) {
-      switch (e.key) {
-        case 's':
-          backward = 0
-          break
-        case 'w':
-          forward = 0
-          break
-        case 'a':
-          left = 0
-          break
-        case 'd':
-          right = 0
-          break
-        default:
-          break
-      }
-    }
-  </script>
-  <svelte:window
-    on:keydown|preventDefault={onKeyDown}
-    on:keyup={onKeyUp}
-  />
-  <T.Group position.x={position[0]/100} position.y={0.9} position.z={position[2]/100}>
-    <T.PerspectiveCamera
+  }
+
+  renderer.domElement.addEventListener('click', lockControls)
+</script>
+
+<svelte:window on:keydown={handleKeyDown} 
+                on:keyup={handleKeyUp} 
+                on:mousedown={handleMouseDown}
+                on:mouseup={handleMouseUp}/>
+<div class="progress-container">
+<div class="progress-bar" style="width: 100%"> {progressWidth}</div>
+</div>
+
+<T.Group {position}>
+  <T.PerspectiveCamera
       makeDefault
       fov={90}
       bind:ref={cam}
-      position.x={position[0]}
-      position.y={position[1]}
-      position.z={position[2]}
-      on:create={({ ref }) => {
-        ref.lookAt(new Vector3(0, 2, 0))
-      }}
-    >
-      <PointerLockControls bind:lock />
-    </T.PerspectiveCamera>
-  </T.Group>
-  <T.Group {position}>
-    <RigidBody
-      bind:rigidBody
-      {position}
-      enabledRotations={[false, false, false]}
-    >
-      <CollisionGroups groups={[0]}>
-        <Collider
-          shape={'capsule'}
-          args={[height / 2 - radius, radius]}
-        />
-      </CollisionGroups>
-      <CollisionGroups groups={[15]}>
-        <T.Group position={[0, -height / 2 + radius, 0]}>
-          <Collider
-            sensor
-            shape={'ball'}
-            args={[radius * 1.2]}
-          />
-        </T.Group>
-      </CollisionGroups>
-    </RigidBody>
-    <Gun position={[.75,.5,-1]}/>
-  </T.Group>
+      position={[0, 1.7, 0]}
+      enabledRotations={[true, false, true]}
+  >
+      <PointerLockControls />
+      <Gun position={new Vector3(.8, -.65,-.75)}/>
+
+  </T.PerspectiveCamera>
+  
+  <RigidBody bind:rigidBody {position} type="dynamic" enabledRotations={[false, false, false]}>
+  <CollisionGroups groups={[0]}>
+          <Collider shape={'capsule'} args={[0.85, 0.3]} />
+  </CollisionGroups>
+  <CollisionGroups groups={[15]}>
+          <T.Group position={[0, -0.85, 0]}>
+              <Collider sensor shape={'ball'} args={[0.36]} />
+    </T.Group>
+  </CollisionGroups>
+      <T.Mesh castShadow>
+          <T.BoxGeometry args={[1, 2, 1]} />
+          <T.MeshStandardMaterial color="blue" />
+      </T.Mesh>
+</RigidBody>
+</T.Group>
+
+<style>
+.progress-container {
+  width: 100%;
+  height: 10px; /* Increased height for visibility */
+  background-color: rgba(240, 240, 240, 0.5); /* Semi-transparent background */
+  position: fixed; /* Changed to fixed */
+  top: 0;
+  left: 0;
+  z-index: 1000;
+  pointer-events: none; /* Allows clicks to pass through */
+}
+
+.progress-bar {
+  height: 100%;
+  background-color: #4CAF50;
+  transition: width 0.3s ease;
+}
+</style>
